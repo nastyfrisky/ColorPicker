34f74756aa01376e5b27c0feddba554f4d09ffbd
Nastyfrisky <stpnkvnst@mail.ru>
2021-12-19 22:54:39 +0300

    Логика работы цветовой палитры

--- ColorPicker/ViewController.swift
+++ ColorPicker/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var selectedColorView: UIView!
+    
+    @IBOutlet var redColorLabel: UILabel!
+    @IBOutlet var greenColorLabel: UILabel!
+    @IBOutlet var blueColorLabel: UILabel!
+    
+    var redValue: CGFloat = 0.5
+    var greenValue: CGFloat = 0.5
+    var blueValue: CGFloat = 0.5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBAction func redComponentSlider(_ sender: UISlider) {
+        redColorLabel.text = String(format: "%.2f", sender.value)
+        redValue = CGFloat(sender.value)
+        setViewColor()
+    }
+    
+    @IBAction func greenComponentSlider(_ sender: UISlider) {
+        greenColorLabel.text = String(format: "%.2f", sender.value)
+        greenValue = CGFloat(sender.value)
+        setViewColor()
+    }
+    
+    @IBAction func blueComponentSlider(_ sender: UISlider) {
+        blueColorLabel.text = String(format: "%.2f", sender.value)
+        blueValue = CGFloat(sender.value)
+        setViewColor()
+    }
+   
+    
+    private func setViewColor() {
+        selectedColorView.backgroundColor = UIColor(
+            displayP3Red: redValue,
+            green: greenValue,
+            blue: blueValue,
+            alpha: 1
+        )
     }
 
-
 }
+
 
