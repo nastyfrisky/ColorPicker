cc6ed8382010cba862fcb6695c5707ebc89c8c7b
Nastyfrisky <stpnkvnst@mail.ru>
2022-01-24 20:24:04 +0300

    wip

--- ColorPicker/ViewControllers/PickerViewController.swift
+++ ColorPicker/ViewControllers/PickerViewController.swift
@@ -7,7 +7,9 @@
 
 import UIKit
 
-class PickerViewController: UIViewController {
+class PickerViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: - IB Outlets
 
     @IBOutlet var selectedColorView: UIView!
     
@@ -19,6 +21,12 @@ class PickerViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
+    @IBOutlet var redTextField: UITextField!
+    @IBOutlet var greenTextField: UITextField!
+    @IBOutlet var blueTextField: UITextField!
+    
+    // MARK: - Override Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,13 +37,29 @@ class PickerViewController: UIViewController {
             alpha: 1
         )
         
+        setupTextFields()
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            super.touchesBegan(touches, with: event)
+            view.endEditing(true)
+        }
+    
+    // MARK: - IB Actions
+    
     @IBAction func rgbComponentsSlider() {
         redColorLabel.text = String(format: "%.2f", redSlider.value)
         greenColorLabel.text = String(format: "%.2f", greenSlider.value)
         blueColorLabel.text = String(format: "%.2f", blueSlider.value)
+        
+        redTextField.text = String(format: "%.2f", redSlider.value)
+        greenTextField.text = String(format: "%.2f", greenSlider.value)
+        blueTextField.text = String(format: "%.2f", blueSlider.value)
+        
         setViewColor()
     }
+    
+    // MARK: - Private methods
     
     private func setViewColor() {
         selectedColorView.backgroundColor = UIColor(
@@ -45,7 +69,31 @@ class PickerViewController: UIViewController {
             alpha: 1
         )
     }
-
+    private func setupTextFields() {
+        let bar = UIToolbar()
+        let leftSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(toolbarDoneTapped))
+        bar.items = [leftSpace, doneButton]
+        bar.sizeToFit()
+        
+        redTextField.inputAccessoryView = bar
+        greenTextField.inputAccessoryView = bar
+        blueTextField.inputAccessoryView = bar
+    }
+    
+    @objc private func toolbarDoneTapped() {
+        redColorLabel.text = redTextField.text
+        greenColorLabel.text = greenTextField.text
+        blueColorLabel.text = blueTextField.text
+        
+        redSlider.value = Float(redTextField.text ?? "1.00") ?? 1.00
+        greenSlider.value = Float(greenTextField.text ?? "1.00") ?? 1.00
+        blueSlider.value = Float(blueTextField.text ?? "1.00") ?? 1.00
+        
+        setViewColor()
+        
+        view.endEditing(true)
+    }
 }
 
 
